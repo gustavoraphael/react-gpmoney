8e8aea26a3750cce1e9b620b7f01bc882a75de9c
Gustavo Raphael <gustavo.raphael@totvs.com.br>
2021-04-21 00:18:58 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+
+export function App() {
+    return (
+        <>
+            <Header />
+            <GlobalStyle />
+        </>
+    );
+}
